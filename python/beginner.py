46013e6ca0aa7eaa99929eb18cfda48ca83713c8
Anugna15 <anusomepallii@gmail.com>
2025-05-10 19:12:37 +0530

    ml

--- python/beginner.py
+++ python/beginner.py
@@ -53,14 +53,7 @@ b=float(input("Enter b: "))
 c=float(input("Enter c: "))
 delta=b**2-4*a*c
 print("Delta: ", delta)
-#conditionals
-num=int(input("Enter a number: "))
-if num>0:
-    print("Positive")
-elif num<0:
-    print("Negative")
-else:
-    print("Zero")
+
 #comparisons
 a=10
 b=20
@@ -78,4 +71,61 @@ print(a or b)#a or b
 print(a is b)#a is b
 print(not a)#not a
 
+#conditionals
+num=int(input("Enter a number: "))
+if num>0:
+    print("Positive")
+elif num<0:
+    print("Negative")
+else:
+    print("Zero")
 
+#Make a program that asks for a person's age and displays whether they are of legal age or not
+age=int(input("Enter your age: "))
+if age>=18:
+    print("You are of legal age.")
+else:
+    print("You are not of legal age.")
+#Make a program that asks for a number and displays whether it is even or odd
+num=int(input("Enter a number: "))
+if num%2==0:
+    print("Even")
+else:
+    print("Odd")
+#Write a program that reads two numbers and tells you which one is bigger.
+a=int(input("enter a 1st number: "))
+b=int(input("enter 2nd number: "))
+if a>b:
+    print(a,"is bigger than",b)
+elif b>a:
+    print(b,"is bigger than",a)
+else:
+    print(a,"is equal to",b)
+
+#Write a program that asks the user for three numbers and displays the largest one
+a=int(input("enter 1st number: "))
+b=int(input("enter 2nd number: "))
+c=int(input("enter 3rd number: "))
+if a>b & a>c:
+    print(a,"is the largest number")
+elif b>a & b>c:
+    print(b,"is the largest number")
+else:
+    print(c,"is the largest number")
+#Make a program that reads the grades of two tests, calculates the simple arithmetic mean, and informs whether the student passed (average greater than or equal to 6) or failed (average less than 6)
+test1=float(input("Enter the grade of the 1st test: "))
+test2=float(input("Enter the grade of the 2nd test: "))
+average=(test1+test2)/2
+if average>=6:
+    print("You passed")
+else:
+    print("You failed")
+#Make a program that reads three numbers, and informs if their sum is divisible by 5 or not
+a=int(input("Enter 1st number: "))
+b=int(input("Enter 2nd number: "))
+c=int(input("Enter 3rd number: "))
+sum=a+b+c
+if sum%5==0:
+    print("The sum is divisible by 5")
+else:
+    print("The sum is not divisible by 5")

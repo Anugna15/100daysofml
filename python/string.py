b8f68f051f3d9969d2575767dcf47e680698e424
Anugna15 <anusomepallii@gmail.com>
2025-05-21 16:46:59 +0530

    strings

---
+++ python/string.py
@@ -0,0 +1,22 @@
+my_string="Hello world!"
+print(my_string)
+
+#accessing characters
+print(my_string[2])
+
+#slicing
+print(my_string[:5])
+
+#string concatenation
+FirstName="Anugna"
+LastName="Somepalli"
+Fullname=FirstName+" "+LastName
+print(Fullname)
+
+#string length
+length=len(Fullname)
+print(length)
+
+print("lower case of string is",Fullname.lower())
+print("upper case of string is",Fullname.upper())
+print(my_string.find("d"))
